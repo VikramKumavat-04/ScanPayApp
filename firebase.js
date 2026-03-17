eb1139aca70af85b551257d0ce1582c0a2dff62d
Vikram Kumavat <vikramkumavat2004@gmail.com>
2026-03-17 18:02:01 +0530

    Remove Firebase config from firebase.js
    Removed sensitive Firebase configuration details.

--- firebase.js
+++ firebase.js
@@ -4,12 +4,7 @@ import { initializeAuth, getAuth, getReactNativePersistence } from 'firebase/aut
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBduxEyIkhZvEmE0cl1alsWKFujixxGx9g",
-  authDomain: "scanpayapp-5f704.firebaseapp.com",
-  projectId: "scanpayapp-5f704",
-  storageBucket: "scanpayapp-5f704.firebasestorage.app",
-  messagingSenderId: "383166108649",
-  appId: "1:383166108649:web:64fef456f22fd91290b178"
+ 
 };
 
 let app;
